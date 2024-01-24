1fe870b7d570aafc8acc1eae1bb62c8a074e131e
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-01-24 15:51:20 -0300

    Criada classe Medico e Endereco

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,27 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.enums.Especialidade;
+
+@Data
+@EqualsAndHashCode(of = "id")
+@Table(name = "medicos")
+@Entity
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+}
