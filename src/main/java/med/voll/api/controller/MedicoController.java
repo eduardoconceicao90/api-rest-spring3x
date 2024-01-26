96f4ed06059d1a85e420069dc91fb3e40c6e6664
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-01-26 10:31:03 -0300

    Adicionada validações para entidade Medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.repository.MedicoRepository;
@@ -17,7 +18,7 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
-    public void Cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void Cadastrar(@Valid @RequestBody DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
 }
