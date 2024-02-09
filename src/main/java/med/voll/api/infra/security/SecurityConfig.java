b0cc0f5f372c4402878ca8bdaae8fe8e2dce7ae3
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-02-09 13:05:44 -0300

    Documentando com SpringDoc

--- src/main/java/med/voll/api/infra/security/SecurityConfig.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                    .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                    .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/v3/api-docs/**","/swagger-ui.html", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                    )
                    .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
