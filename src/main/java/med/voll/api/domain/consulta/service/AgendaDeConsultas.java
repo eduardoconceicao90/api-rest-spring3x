21124ec0086c8d44fa1009f7a52970f99780f82c
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-02-09 07:27:34 -0300

    Criado service de agendamento de consultas

---
+++ src/main/java/med/voll/api/domain/consulta/service/AgendaDeConsultas.java
@@ -0,0 +1,31 @@
+package med.voll.api.domain.consulta.service;
+
+import med.voll.api.domain.consulta.Consulta;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.repository.ConsultaRepository;
+import med.voll.api.domain.medico.repository.MedicoRepository;
+import med.voll.api.domain.paciente.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultas {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public void agendar(DadosAgendamentoConsulta dados){
+
+        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var consulta = new Consulta(null, medico, paciente, dados.data());
+
+        consultaRepository.save(consulta);
+    }
+}
